1335f1ff6f5b9d53db057831b399a0c03b0177b4
Nikhil Chavan <chavannikhil1007@gmail.com>
2022-10-16 17:10:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,109 @@
+import React from 'react';
+
+import Card from "./Card";
+
+
+const App = () => {
+  return (
+    <>
+    <div>
+    <div className="container">
+     <h1 className="heading1"> <u>Top Shows</u></h1>
+      <div className="row">
+      <Card imgsrc="https://m.media-amazon.com/images/M/MV5BOTk2NzUyOTctZDdlMS00MDJlLTgzNTEtNzQzYjFhNjA0YjBjXkEyXkFqcGdeQXVyMjg1NDcxNDE@._V1_.jpg"
+            title="Web Series"
+            sname="Dark"
+            link="http://www.netflix.com/in/title/80100172?source=35"/>
+
+<Card imgsrc="https://flxt.tmsimg.com/assets/p185846_b_v8_ad.jpg"
+            title="Web Series"
+            sname="Breaking Bad"
+            link="https://www.netflix.com/in/title/70143836?source=35"/>
+
+<Card imgsrc="https://m.media-amazon.com/images/M/MV5BNjgxZTMxNmQtZGFkZS00NDUyLTllMzYtOTE0ZjdlMjM1ZWU0XkEyXkFqcGdeQXVyMTAyMTE1MDA1._V1_FMjpg_UX1000_.jpg"
+            title="Web Series"
+            sname=" Scam 1992"
+            link="https://www.sonyliv.com/shows/scam-1992-the-harshad-mehta-story-marathi-1700000845?utm_source=Google&utm_medium=WatchNow&utm_campaign=1700000845"/>
+            </div>
+           </div>
+    </div>
+      
+
+    <div>
+    <div className="container" style={{margin:"0px"}}>
+     <h1 className="heading1" > </h1>
+      <div className="row">
+      <Card imgsrc="https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"
+            title="Movie"
+            sname="Interstellar"
+            link="https://www.netflix.com/in/title/70305903?source=35"/>
+
+<Card imgsrc="https://m.media-amazon.com/images/M/MV5BYTdkMWVjNWQtNmY3Ni00OTVjLTllZTUtNzYyMjAzN2EwYWM5XkEyXkFqcGdeQXVyMjA3NzQyMA@@._V1_QL75_UY281_CR17,0,190,281_.jpg"
+            title="Movie"
+            sname="The Big Short"
+            link="https://www.primevideo.com/dp/amzn1.dv.gti.84b49af0-cc89-50c5-1752-b75eaf031f3d?autoplay=0&ref_=atv_cf_strg_wb"/>
+
+<Card imgsrc="https://upload.wikimedia.org/wikipedia/en/thumb/1/13/Top_Gun_Maverick_Poster.jpg/220px-Top_Gun_Maverick_Poster.jpg"
+            title="Movie"
+            sname=" Top Gun: Maverick"
+            link="https://www.primevideo.com/dp/amzn1.dv.gti.e6867686-3989-4e51-ad7b-19ff5edea2f4?autoplay=0&ref_=atv_cf_strg_wb"/>
+            </div>
+           </div>
+    </div>
+
+    <div>
+    <div className="container" style={{margin:"0px"}}>
+     <h1 className="heading1" > </h1>
+      <div className="row">
+      <Card imgsrc="https://staticg.sportskeeda.com/editor/2021/10/8bbb3-16349088266046-1920.jpg"
+            title="Anime"
+            sname="Naruto: Shippuden"
+            link="https://www.netflix.com/in/title/80000603?source=35"/>
+
+<Card imgsrc="https://www.whatspaper.com/wp-content/uploads/2021/10/hd-attack-on-titan-wallpaper-whatspaper-2.jpg"
+            title="Anime"
+            sname="Attack on Titan"
+            link="https://www.hotstar.com/my/tv/attack-on-titan/1260059935"/>
+
+<Card imgsrc="https://cdn.verasia.eu/19915-large_default/jujutsu-kaisen-tv-anime-1st-season-complete-book.jpg"
+            title="Anime"
+            sname=" Jujutsu Kaisen"
+            link="https://www.netflix.com/in/title/81278456?source=imdb"/>
+            </div>
+           </div>
+    </div>
+
+    <div>
+    <div className="container" style={{margin:"0px"}}>
+     <h1 className="heading1" > </h1>
+      <div className="row">
+      <Card imgsrc="https://rukminim1.flixcart.com/image/416/416/j6qs9e80/movie/y/z/z/2014-dvd-warner-brothers-english-tom-and-jerry-show-season-1-original-imaex3vtkpzsernw.jpeg?q=70"
+            title="Cartoon"
+            sname="The Tom and Jerry Show"
+            link="https://www.primevideo.com/dp/amzn1.dv.gti.eef9d21f-600a-4c76-930f-6ba8c2e8f2e2?autoplay=0&ref_=atv_cf_strg_wb"/>
+
+<Card imgsrc="https://pbs.twimg.com/media/Erh88m6XIAUfBMU.jpg"
+            title="Cartoon"
+            sname="Supa Strikas"
+            link="https://www.netflix.com/in/title/81330518?source=35"/>
+
+<Card imgsrc="https://m.media-amazon.com/images/I/91iyTpTXFOL._SX300_.jpg"
+            title="Cartoon"
+            sname=" Slugterra"
+            link="https://www.primevideo.com/detail/Slugterra/0J3QP92964MF0FKQ3A77C2HFD6"/>
+            </div>
+           </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
---
+++ src/Card.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { useState } from "react";
+import { useReducer } from "react";
+import "./Card.css";
+
+const Card = (props) => {
+    console.log(props);
+
+    // useState for Like's & View's 
+    const [view , setView] = useState(null);
+    const [like , setLike] = useState(null);
+    const handelView = () => {setView(view + 1)};
+    const handelLike = () => {setLike(like + 1)};
+
+    
+
+      // useReducer for rating bar
+    const initialState = { width: 25 };
+    const reducer = (state, action) => {
+    switch (action) {
+    case 'plus':
+      return { width: state.width + 50 }
+    case 'minus':
+      return { width: Math.max(state.width - 50, 2) }
+    default:
+      throw new Error("what's going on?" )
+  }
+}
+const [state, dispatch] = useReducer(reducer, initialState);
+
+    
+  return (
+    <>
+ 
+      
+      <div className="cards col__3">
+        <div className="card  ">
+          <img src={props.imgsrc} alt="pic" className="card__img" />
+          <div className="card__info">
+            <h3 className="card__title" style={{margin:'auto' , font:'caption'}} >{props.sname}</h3>
+            <span className="card__category" style={{font:'small-caption'}} ><u>{props.title}</u></span>
+
+         {/*   <div className="like-view">
+
+            <div >
+            <button className="btn"  style={{backgroundColor:"black", color:"white"}} onClick={ () => {
+              if (like > 0) setLike(null)
+              else if(like < 1) setLike(like + 1)
+            }}  > 
+            <img style={{height:"30px", width:"30px", backgroundColor:"black"}} src="https://cdn-icons-png.flaticon.com/512/1029/1029183.png"  />
+            </button>
+            </div>likes {like}
+            
+            <div>
+             <p className="p">VIEW'S {view} </p>
+            </div>
+            </div>
+          */}
+          
+              <button style={{marginBottom:'15px', padding:"4px", marginInline:"90px"}} className="btn-watch" onClick={handelView}>  <a style={{color:"white", textDecoration:"none"}} href={props.link} target="_blank">Watch Now </a></button>
+           
+
+            {/* usereducer rating bar */}
+           {/* <div style={{ background: 'yellow', height: '7px', width: state.width ,maxWidth: '250px'}}></div>
+              <div style={{marginTop: '0.5rem'}}>
+                  <button style={{background: 'black' }} onClick={() => dispatch('plus')}>👍</button>
+                  <button style={{background: 'black' , marginLeft:'180px' }} onClick={() => dispatch('minus')}>👎</button>
+          </div>*/}
+
+          </div>
+        </div>
+      </div>
+   
+
+    </>
+    
+  );
+    
+}
+
+export default Card;
